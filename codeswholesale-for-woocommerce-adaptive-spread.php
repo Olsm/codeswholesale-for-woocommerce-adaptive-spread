81f621005734c6c172c8f02459bec56f2177a25f
OlavOlsm <OlavOlsm@gmail.com>
2017-11-04 16:15:10 +0100

    customize title so it appears with codeswholesale plugin

--- codeswholesale-for-woocommerce-adaptive-spread.php
+++ codeswholesale-for-woocommerce-adaptive-spread.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Adaptive spread for CodesWholesale for WooCommerce
+Plugin Name: CodesWholesale for WooCommerce Adaptive spread
 Description: Automatic spread based on product prices for CodesWholesale for WooCommerce
 Version: 1.0.0
 Author: Olav Småriset
