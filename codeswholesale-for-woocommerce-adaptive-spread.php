4aa7aafa16ecfe5f63ee2abc0bc79966067e5560
OlavOlsm <OlavOlsm@gmail.com>
2017-11-09 19:39:06 +0100

    Fix filter for calculateSpreadCustom takes 3 params and tweak price spreads

--- codeswholesale-for-woocommerce-adaptive-spread.php
+++ codeswholesale-for-woocommerce-adaptive-spread.php
@@ -6,34 +6,40 @@ Version: 1.0.0
 Author: Olav Småriset
 */
 
-add_filter('codeswholesale_calculate_spread', 'calculateSpreadCustom', 10, 2);
+add_filter('codeswholesale_calculate_spread', 'calculateSpreadCustom', 10, 3);
 
 /**
  * Update price and stock.
  *
  * When CW send request or when post is updated.
- * @param double $priceSpread
+ * @param double $priceWithSpread
+ * @param array $spreadParams
  * @param double $price
- * @return double
+ * @return float
  */
-function calculateSpreadCustom($priceSpread, $price) {
-    if ($price>=50) {
+function calculateSpreadCustom($priceWithSpread, $spreadParams, $price) {
+    // Return price with fixed spread if spread type is not adaptive
+    if ($spreadParams['cwSpreadType'] == 0) {
+        return $priceWithSpread;
+    }
+
+    if ($price>=45) {
         $priceSpread = 5;
-    } elseif ($price >= 45) {
-        $priceSpread = 10;
     } elseif ($price >= 40) {
-        $priceSpread = 15;
+        $priceSpread = 10;
     } elseif ($price >= 35) {
-        $priceSpread = 20;
+        $priceSpread = 15;
     } elseif ($price >= 30) {
-        $priceSpread = 25;
+        $priceSpread = 20;
     } elseif ($price >= 25) {
-        $priceSpread = 30;
+        $priceSpread = 25;
     } elseif ($price >= 20) {
-        $priceSpread = 35;
+        $priceSpread = 30;
     } elseif ($price >= 15) {
-        $priceSpread = 40;
+        $priceSpread = 35;
     } elseif ($price >= 10) {
+        $priceSpread = 40;
+    } elseif ($price >= 5) {
         $priceSpread = 45;
     } elseif ($price < 5) {
         $priceSpread = 50;
